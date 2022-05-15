f9aac7a7648dfdc1265b450e516989b66a496946
antonshishkin92 <antonshishkin92@gmail.com>
2022-05-15 14:33:06 +0400

    add project Anton

---
+++ src/main/java/antonProject/registryRunner.java
@@ -0,0 +1,11 @@
+package antonProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class registryRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(registryRunner.class,args);
+    }
+}
---
+++ src/main/java/antonProject/repository/CarRepository.java
@@ -0,0 +1,8 @@
+package antonProject.repository;
+
+import antonProject.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepository extends JpaRepository<Car,Integer> {
+
+}
